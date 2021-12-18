d110390379143471dae2d241a90c45e8e1ffc231
Rana Sam <ranausama2082001@gmail.com>
2021-12-19 00:26:00 +0500

    program 5

--- script.js
+++ script.js
@@ -38,3 +38,23 @@ function monthName(x)
 }
 
 monthName('1/10/2009')
+
+// program 5
+
+function dates(d1,d2) {
+    var date1=new Date(d1)
+    var date2=new Date(d2)
+    if (date1>date2) {
+        console.log("Date1 > Date2");
+    }
+    else if (date1<date2) {
+        console.log("Date1 < Date2");
+    }
+    else{
+        console.log("Date1 = Date2");
+
+    }
+    
+}
+dates('11/14/2013 00:00','11/14/2013 00:00')
+dates('11/14/2013 00:01','11/14/2013 00:00')
