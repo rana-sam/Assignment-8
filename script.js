460289cf907ded06610a154342ae22970da0a9a4
Rana Sam <ranausama2082001@gmail.com>
2022-01-01 22:47:34 +0500

    date

--- script.js
+++ script.js
@@ -1,88 +1,256 @@
+// Write a JavaScript function to get the minimum date from an array of dates.   
+
+// Test Data :
+// console.log(min_date(['2015/02/01', '2015/02/02', '2015/01/03']));
+// Output :
+// "2015/01/03"
+
+function min_date(all_dates) {
+    var min_dt = all_dates[0],
+     min_dtObj = new Date(all_dates[0]);
+    all_dates.forEach(function(dt, index)
+     {
+     if ( new Date( dt ) < min_dtObj)
+     {
+     min_dt = dt;
+     min_dtObj = new Date(dt);
+     }
+     });
+    return min_dt;
+     }
+   console.log(min_date(['2015/02/01', '2015/02/02', '2015/01/03']));
+
+   
+   //Write a JavaScript function that will return the number of minutes in hours and minutes.   
+
+// Test Data :
+// console.log(timeConvert(200));
+// Output :
+// "200 minutes = 3 hour(s) and 20 minute(s)."
 
 
-
-// program 2
-
-function currentDate(x) {
-    var d= new Date()
-    var date=d.getDate()
-    var month=d.getMonth()+1
-    var year=d.getFullYear()
-    var current=date + x + month + x  +year
-    console.log(current);
+function timeConvert(n) {
+    var num = n;
+    var hours = (num / 60);
+    var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.round(minutes);
+    return num + " minutes = " + rhours + " hour(s) and " + rminutes + " minute(s).";
+    }
     
-}
- currentDate("/");
+    console.log(timeConvert(200));
 
-
-//  program 3
-
-function Days(x,y) {
-
-    return new Date(x, y, 0).getDate();
     
-}
- var x=Days(2021,2)
- console.log(x);
- 
-//  program 4
+    //Write a JavaScript function to get the amount of days of a year.   
 
-function monthName(x) 
+// Test Data :
+// console.log(days_of_a_year(2015));
+// 365
+// console.log(days_of_a_year(2016));
+// 366
+
+function days_of_a_year(year) 
 {
-    
-    var d=new Date(x)
-    var month=d.getMonth()
-   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    console.log(months[month]);
-    
+   
+  return isLeapYear(year) ? 366 : 365;
 }
 
-monthName('1/10/2009')
-
-// program 5
-
-function dates(d1,d2) {
-    var date1=new Date(d1)
-    var date2=new Date(d2)
-    if (date1>date2) {
-        console.log("Date1 > Date2");
-    }
-    else if (date1<date2) {
-        console.log("Date1 < Date2");
-    }
-    else{
-        console.log("Date1 = Date2");
-
-    }
-    
+function isLeapYear(year) {
+     return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
 }
-dates('11/14/2013 00:00','11/14/2013 00:00')
-dates('11/14/2013 00:01','11/14/2013 00:00')
+
+console.log(days_of_a_year(2015));
+console.log(days_of_a_year(2016));
 
 
-// program 6
-function addMinute(mint) {
-    var d=new Date(mint)
-    d.setMinutes(mint)
-    console.log(d);
+// Write a JavaScript function to get the quarter (1 to 4) of the year.   
+
+// Test Data :
+// console.log(quarter_of_the_year(new Date(2015, 1, 21)));
+// 2
+// console.log(quarter_of_the_year(new Date(2015, 10, 18)));
+
+function quarter_of_the_year(date) 
+  {
+    var month = date.getMonth() + 1;
+    return (Math.ceil(month / 3));
+  }
+
+
+console.log(quarter_of_the_year(new Date()));
+console.log(quarter_of_the_year(new Date(2015, 1, 21))); 
+console.log(quarter_of_the_year(new Date(2015, 10, 18)));
+
+
+// Write a JavaScript function to count the number of days passed since beginning of the year.   
+
+// Test Data :
+// console.log(days_passed(new Date(2015, 0, 15)));
+// 15
+// console.log(days_passed(new Date(2015, 11, 14)));
+// 348
+
+function days_passed(dt) {
+    var current = new Date(dt.getTime());
+    var previous = new Date(dt.getFullYear(), 0, 1);
+  
+    return Math.ceil((current - previous + 1) / 86400000);
+  }
+  console.log(days_passed(new Date(2015, 0, 15)));
+  console.log(days_passed(new Date(2015, 11, 14)));
+  
+
+//   Write a JavaScript function to convert a Unix timestamp to time.   
+
+//   Test Data :
+//   console.log(days_passed(new Date(2015, 0, 15)));
+//   15
+//   console.log(days_passed(new Date(2015, 11, 14)));
+//   348
+
+ 
+function Unix_timestamp(t)
+{
+var dt = new Date(t*1000);
+var hr = dt.getHours();
+var m = "0" + dt.getMinutes();
+var s = "0" + dt.getSeconds();
+return hr+ ':' + m.substr(-2) + ':' + s.substr(-2);  
 }
-addMinute(50)
 
+console.log(Unix_timestamp(1412743274));
+// Write a JavaScript function to get 12-hour format of an hour with leading zeros.   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(hours_with_zeroes(dt));
+// "12"
 
-// program 7
-
-function isWeekend(dat) {
-    var d=new Date(dat)
-    var week=d.getDay()
-    if (week==0|| week==6) {
-        
-        console.log("Weekend");
-    }
-    else{
-        console.log("undefinrd");
-    }
-    
+function hours_with_zeroes(dt) 
+{ 
+  return ((dt.getHours() % 12 || 12) < 10 ? '0' : '') + (dt.getHours() % 12 || 12);
 }
-isWeekend('Nov 15, 2014')
-isWeekend('Nov 16, 2014')
-isWeekend('Nov 17, 2014')
+
+dt = new Date(); 
+console.log(hours_with_zeroes(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(hours_with_zeroes(dt));
+
+
+// Write a JavaScript function to get 24-hour format of an hour without leading zeros.   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(hours_without_zeroes(dt));
+// 0]
+
+
+function hours_without_zeroes(dt) 
+{ 
+  return dt.getHours();
+}
+
+dt = new Date(); 
+console.log(hours_without_zeroes(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(hours_without_zeroes(dt));
+
+
+// Write a JavaScript function to get minutes with leading zeros (00 to 59).   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(minutes_with_leading_zeros(dt));
+// "00"
+
+
+function minutes_with_leading_zeros(dt) 
+{ 
+  return (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
+}
+
+dt = new Date(); 
+console.log(minutes_with_leading_zeros(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(minutes_with_leading_zeros(dt));
+
+
+// Write a JavaScript function to get seconds with leading zeros (00 through 59).   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(seconds_with_leading_zeros(dt));
+// "00"
+
+function seconds_with_leading_zeros(dt) 
+{ 
+  return (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
+}
+
+dt = new Date(); 
+console.log(seconds_with_leading_zeros(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(seconds_with_leading_zeros(dt));
+
+
+
+// Write a JavaScript function to get Timezone.   
+// Test Data :
+// dt = new Date();
+// console.log(seconds_with_leading_zeros(dt));
+// "India Standard Time"
+
+function seconds_with_leading_zeros(dt) 
+{ 
+  return /\((.*)\)/.exec(new Date().toString())[1];
+}
+
+dt = new Date(); 
+console.log(seconds_with_leading_zeros(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(seconds_with_leading_zeros(dt));
+
+// Write a JavaScript function to get lowercase Ante meridiem and Post meridiem.
+
+function lower_am_pm(dt) 
+{ 
+   return dt.getHours() < 12 ? 'AM' : 'PM';
+}
+
+dt = new Date(); 
+console.log(lower_am_pm(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(lower_am_pm(dt));
+
+
+
+// 31. Write a JavaScript function to get uppercase Ante meridiem and Post meridiem.   function upper_am_pm(dt) 
+{ 
+    return dt.getHours() < 12 ? 'AM' : 'PM';
+  }
+  
+  dt = new Date(); 
+  console.log(upper_am_pm(dt)); 
+  
+  dt = new Date(1989, 10, 1); 
+  console.log(upper_am_pm(dt));
+
+  
+//   Write a JavaScript function to swatch Internet time (000 through 999).   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(internet_time(dt));
+// 812
+
+function internet_time(dt) 
+{ 
+  return Math.floor((((dt.getUTCHours() + 1) % 24) + dt.getUTCMinutes() / 60 + dt.getUTCSeconds() / 3600) * 1000 / 24);
+}
+
+dt = new Date(); 
+console.log(internet_time(dt)); 
+
+dt = new Date(1989, 10, 1); 
+console.log(internet_time(dt));
